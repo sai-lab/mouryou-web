fb1add401e9b4f97629915d2678d3efbd2e12543
Akihiko Horiuchi <12ff5b8@gmail.com>
2015-10-26 12:44:00 +0900

    VMをリスト表示 / アニメーションを追加

--- app/app.js
+++ app/app.js
@@ -4,6 +4,18 @@ var extractStatus = function(xhr, xhrOptions) {
   return xhr.status !== 200 ? xhr.status : xhr.responseText;
 };
 
+var animationDelay = function(time) {
+  var delay = String(time * 0.1) + 's';
+
+  return {
+    '-moz-animation-delay': delay,
+    '-webkit-animation-delay': delay,
+    '-o-animation-delay': delay,
+    '-ms-animation-delay': delay,
+    'animation-delay': delay
+  };
+};
+
 (function(window) {
   'use strict';
 
--- app/view/dashboard.js
+++ app/view/dashboard.js
@@ -6,12 +6,37 @@ var dashboard = dashboard || {};
   dashboard.view = function(ctrl) {
     document.title = 'mouryou';
 
-    return [
-      m('.chart', [
-        m('#chart-block', {
-          config: dashboard.plotter(ctrl)
-        })
+    return m('.row', [
+      m('.col-md-9', [
+        m('.chart', [
+          m('#chart-block.m-a-sm', {
+            config: dashboard.plotter(ctrl)
+          })
+        ])
+      ]),
+      m('.col-md-3', [
+        m('ul.list-group.small', [
+          ctrl.hypervisors().map(function(hypervisor) {
+            return [
+              m('li.list-group-item.bg-mouryou.text-white.border-mouryou.p-a-sm.animated.fadeInRight', [
+                m('i.fa.fa-fw.fa-server.m-r-sm'),
+                m('span.bold', hypervisor.host)
+              ]),
+              hypervisor.virtual_machines.map(function(machine, i) {
+                return m('li.list-group-item.p-a-sm.animated.fadeInRight', {
+                  style: animationDelay(i)
+                }, [
+                  m('i.fa.fa-fw.fa-hdd-o.m-r-sm'),
+                  m('span.bold', machine.host),
+                  m('span.label.label-pill.bg-default.text-white.pull-right', [
+                    m('i.fa.fa-fw.fa-power-off')
+                  ])
+                ]);
+              })
+            ];
+          })
+        ])
       ])
-    ];
+    ]);
   };
 })();
